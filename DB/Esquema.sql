40b3017d3bc4805f770eaf27b6faeec4f049f001
JesusFuentesGalindo <jesusfuentesgalindo@gmail.com>
2018-06-24 23:21:52 -0500

    20182406 Actualización DB, HTML, PHP
    Se añade formulario HTML: inc
    Se añade modulo PHP: procesar inc
    Se cambia esquema de db

--- DB/Esquema.sql
+++ DB/Esquema.sql
@@ -3,15 +3,20 @@ USE nmc;
 CREATE TABLE incidentes(
 	idIncidente VARCHAR(15) NOT NULL,
     incPadre VARCHAR(15) NULL,
-    asociacion ENUM('Padre', 'Hijo','Sin Relación') NOT NULL,
-    tipoInforme ENUM('Reactivo', 'Proactivo', 'Requerimiento') not null,
-    criticidad ENUM('Crítica', 'Alta', 'Media', 'Baja') NOT NULL,
-    prioridad ENUM('1', '2', '3', '4'),
+    asociacion ENUM('Padre', 'Hijo','Sin Relacion') NOT NULL,
+    tipoInforme ENUM('Reactivo', 'Proactivo', 'Requerimiento') NOT NULL,
+    criticidad ENUM('Critica', 'Alta', 'Media', 'Baja') NOT NULL,
+    prioridad ENUM('1', '2', '3', '4') NOT NULL,
     tipoProblema VARCHAR(60) NOT NULL,
-    inicioAfectacionFalla DATETIME NOT NULL,
-    finAfectacionFalla DATETIME NOT NULL,
-    causa VARCHAR(8000) NOT NULL,
-    resolucion VARCHAR(8000) NOT NULL,
+	causa VARCHAR(8000) NULL,
+    resolucion VARCHAR(8000) NULL,
+    cierreImputable ENUM('Bestel', 'Cliente') NULL,
+    inicioAfectacionInc DATETIME NOT NULL,
+    finAfectacionInc DATETIME NULL,
+    estadoProblema ENUM('Cerrado', 'Abierto', 'Asignado', 'En Curso', 'Pendiente', 'Resuelto') NOT NULL,
+    areaResolutora ENUM('Transporte', 'Tier1', 'Siar', 'Telefonia') NOT NULL,
+    proveedor VARCHAR(50) NULL,
+    afectacion VARCHAR(60) NULL,
     PRIMARY KEY(idIncidente)
 );
 
@@ -19,6 +24,14 @@ CREATE TABLE servicios(
 	
     circuito VARCHAR(100) NOT NULL,
     idCliente INT NOT NULL,
+    servicio VARCHAR(100) NOT NULL,
+    producto VARCHAR(100) NULL,
+    circuitoM6 VARCHAR(100) NULL,
+    circuitoSolar VARCHAR(100) NULL,
+    circuitoSicab VARCHAR(100) NULL,
+    segmento VARCHAR(100) NOT NULL,
+    estadoDestino VARCHAR(100) NOT NULL,
+    sitio VARCHAR(100) NULL,
     PRIMARY KEY(circuito),
     FOREIGN KEY(idCliente) REFERENCES clientes(idCliente)
 );
@@ -26,15 +39,19 @@ CREATE TABLE servicios(
 CREATE TABLE incidenteServicio(
 	idIncidente VARCHAR(15) NOT NULL,
     circuito VARCHAR(100) NOT NULL,
+	inicioAfectacionFalla DATETIME NULL,
+    finAfectacionFalla DATETIME NULL,
     PRIMARY KEY(idIncidente, circuito),
-    FOREIGN KEY(idIncidente) REFERENCES incidentes(idIncidente),
-    FOREIGN KEY(circuito) REFERENCES servicios(circuito)
+    FOREIGN KEY(idIncidente) REFERENCES incidentes(idIncidente) ON UPDATE CASCADE,
+    FOREIGN KEY(circuito) REFERENCES servicios(circuito) ON UPDATE CASCADE
 );
 
 CREATE TABLE clientes(
 	idCliente INT NOT NULL,
     nombreCliente VARCHAR(200) NOT NULL,
     giro ENUM('WHOLESALE', 'INICIATIVA PRIVADA', 'GOBIERNO') NOT NULL,
+    sla INT NOT NULL,
+    empresaGestora ENUM('Bestel', 'Metrored') NOT NULL,
     PRIMARY KEY(idCliente)
 );
 
@@ -44,7 +61,7 @@ CREATE TABLE rfo(
     estatus ENUM('Pendiente', 'En Revisión', 'N/A', 'Enviado') NOT NULL,
     fechaSolicitud DATETIME NOT NULL,
     responsableSolicitud VARCHAR(150) NOT NULL,
-    fechaValidacion DATETIME NOT NULL,
+    fechaEnvioValidacion DATETIME NOT NULL,
     responsableEnvioValidacion VARCHAR(150) NOT NULL,
     responsableValidacion VARCHAR(150) NOT NULL,
     fechaVistoBueno DATETIME NOT NULL,
--- WebContent/php/ingresarINC.php
+++ WebContent/php/ingresarINC.php
@@ -6,11 +6,9 @@
     $criticidad=$_POST['criticidad'];
     $prioridad=$_POST['prioridad'];
     $tipoProblema=$_POST['tipoProblema'];
-    $inicioAfectacionFalla=$_POST['inicioAfectacionFalla'];
-    $finAfectacionFalla=$_POST['finAfectacionFalla'];
     $causa=$_POST['causa'];
     $resolucion=$_POST['resolucion'];
-    $cierreImputable=$_POST['cierreImputable']
+    $cierreImputable=$_POST['cierreImputable'];
     $inicioAfectacionInc=$_POST['inicioAfectacionInc'];
     $finAfectacionInc=$_POST['finAfectacionInc'];
     $estadoProblema=$_POST['estadoProblema'];
@@ -18,11 +16,34 @@
     $proveedor=$_POST['proveedor'];
     $afectacion=$_POST['afectacion'];
     
-    $link=mysqli_connect('localhost', 'administradorlocal', 'AdminMySql');
-    mysqli_select_db('nmc');
+    $link=mysqli_connect('localhost', 'administrador_local', 'AdminMySql', 'nmc');
     
-    $query='';
+    /*$query="INSERT INTO incidentes(`idIncidente`, `incPadre`, `asociacion`, `tipoInforme`, `criticidad`, `prioridad`, `tipoProblema`, `causa`, `resolucion`, `cierreImputable`, `inicioAfectacionInc`, `finAfectacionInc`, `estadoProblema`, `AreaResolutora`, `proveedor`, `afectacion`)
+    VALUES('$idIncidente', '$incPadre', '$asociacion', '$tipoInforme', '$criticidad', '$prioridad', '$tipoProblema', '$causa', '$resolucion', '$cierreImputable', '$inicioAfectacionInc', '$finAfectacionInc', '$estadoProblema', '$areaResolutora', '$proveedor', '$afectacion');";*/
+    
+    $query="INSERT INTO incidentes(`idIncidente`, `incPadre`, `asociacion`, `tipoInforme`, `criticidad`, `prioridad`, `tipoProblema`, `causa`, `resolucion`, `cierreImputable`, `inicioAfectacionInc`, `finAfectacionInc`, `estadoProblema`, `AreaResolutora`, `proveedor`, `afectacion`)
+    VALUES('$idIncidente', '$incPadre', '$asociacion', '$tipoInforme', '$criticidad', '$prioridad', '$tipoProblema', '$causa', '$resolucion', '$cierreImputable', '$inicioAfectacionInc', '$finAfectacionInc', '$estadoProblema', '$areaResolutora', '$proveedor', '$afectacion');";
     $resultado=mysqli_query($link, $query);
-    mysqli_free_result($resultado);
+    
+    //mysqli_free_result($resultado);
     mysqli_close($link);
+    
+    header("Location: ../RFOs/circuitos.html");
+    
+    /*echo "$idIncidente <br />";
+    echo "$incPadre<br />";
+    echo "$asociacion<br />";
+    echo "$tipoInforme<br />";
+    echo "$criticidad<br />";
+    echo "$prioridad<br />";
+    echo "$tipoProblema<br />";
+    echo "$causa<br />";
+    echo "$resolucion<br />";
+    echo "$cierreImputable<br />";
+    echo "$inicioAfectacionInc<br />";
+    echo "$finAfectacionInc<br />";
+    echo "$estadoProblema<br />";
+    echo "$areaResolutora<br />";
+    echo "$proveedor<br />";
+    echo "$afectacion<br />";*/
 ?>
